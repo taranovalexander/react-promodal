080b163ccc6837cbaeae7b9eea2e831c1d3a06b4
Alexander Taranov <alex@cryptosphere-systems.com>
2020-06-22 17:23:44 +0200

    Updates

---
+++ precache-manifest.0d45f20e4fc0996eafb0fc834f6acd77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ab678ff72667230d507d528b53fafc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "48ebb65fdf1a59d4dccd",
+    "url": "./static/js/2.1877393d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1877393d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae151b84d6d621ec1e0d",
+    "url": "./static/js/main.0f3baaf7.chunk.js"
+  },
+  {
+    "revision": "72a43b38a8254d0d980b",
+    "url": "./static/js/runtime-main.26da1059.js"
+  }
+]);
